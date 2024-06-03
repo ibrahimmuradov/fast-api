d6b92555d9a29be692e456d18d2730ebd073fdaa
= <=>
2024-06-03 14:52:07 +0400

    add comments

--- db4.py
+++ db4.py
@@ -4,16 +4,18 @@ from sqlmodel import create_engine, SQLModel, Session
 DATABASE_URL = 'sqlite:///db.sqlite'
 
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=True)  # create_engine - Creates an engine object to interact with
+# the SQLite database, echo=True - enables logging of SQL statements
 
 
 def init_db():
-    SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)  # creates all tables in the database defined by the
+    # SQLModel metadata
 
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+def get_session():  # provides a session context
+    with Session(engine) as session:  # ensure the session is properly closed after use
+        yield session  # used to provide the session to the caller.
 
 
 
--- main4.py
+++ main4.py
@@ -2,16 +2,27 @@ from fastapi import FastAPI, HTTPException, Depends, Path
 from models4 import GenreChoice, BookBase, BookCreate, Book, Publisher
 from contextlib import asynccontextmanager
 from db4 import init_db, get_session
-from sqlmodel import Session
+from sqlmodel import Session, select
 from typing import Annotated
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI):  # An asynchronous context manager to initialize the
+    # database when the application starts
+
     init_db()
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+
+@app.get('/book-list')
+async def book_list(
+        session: Session = Depends(get_session)  # injected database session
+) -> list[Book]:
+    books = session.exec(select(Book)).all()  # select(Book) - query to select all books
+
+    return books
 
 
 @app.get('/book-detail/{book_id}')
@@ -19,7 +30,7 @@ async def book_detail(
         book_id: Annotated[int, Path(name="The Book ID")],
         session: Session = Depends(get_session)
 ) -> Book:
-    book = session.get(Book, id=book_id)
+    book = session.get(Book, book_id) # .get(Book, book_id) - retrieves a specific book by ID.
 
     if book is None:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -34,14 +45,14 @@ async def book_create(
 ) -> Book:
     create_book = Book(name=book_data.name, genre=book_data.genre)
 
-    session.add(create_book)
+    session.add(create_book)  # adds create book to the session
 
     if book_data.publishers:
         for publisher in book_data.publishers:
             publisher_obj = Publisher(name=publisher.name, release_date=publisher.release_date, book=create_book)
             session.add(publisher_obj)
 
-    session.commit()
-    session.refresh(create_book)
+    session.commit()  # commits the transaction.
+    session.refresh(create_book)  # refreshes the create_book instance to reflect the committed state.
 
     return create_book
